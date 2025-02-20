8bd5dbea5fa968053b1e3b12c58ed749402e697f
Brian N <brian.nguyen93@gmail.com>
2025-02-20 19:32:07 +1100

    Add no sandbox to allow chrome driver

--- src/main/java/com/stockaroo/asxnewsapp/scraper/AbstractSiteScraper.java
+++ src/main/java/com/stockaroo/asxnewsapp/scraper/AbstractSiteScraper.java
@@ -11,6 +11,8 @@ public abstract class AbstractSiteScraper implements BaseScraper {
         System.out.println("Creating new driver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new"); // Updated headless mode for Chrome
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
         this.driver = new ChromeDriver(options);
     }
